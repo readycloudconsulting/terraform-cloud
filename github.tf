18716ca0422dc03908c96c9b31cf243d4b6535b5
Jared Ready <jared@jaredready.com>
2022-06-04 13:05:36 -0500

    Fix issues with repo creation

--- github.tf
+++ github.tf
@@ -1,11 +1,7 @@
-data "github_organization" "this" {
-  name = "readycloudconsulting"
-}
-
 resource "github_repository" "this" {
   for_each = local.workspaces
 
-  name               = "readycloudconsulting/${each.key}"
+  name               = each.key
   description        = try(each.value.description, null)
   visibility         = "private"
   gitignore_template = "Terraform"
