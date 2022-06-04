8ed1374ba4d13654f78881e0746e25d7d1a75bee
Jared Ready <jared@jaredready.com>
2022-06-04 12:18:45 -0500

    Add terraform-cloud workspace

--- main.tf
+++ main.tf
@@ -2,3 +2,43 @@ resource "tfe_organization" "this" {
   name  = "ready-cloud-consulting"
   email = "jared@readycloudconsulting.com"
 }
+
+resource "tfe_oauth_client" "this" {
+  name             = "github"
+  organization     = tfe_organization.this.name
+  api_url          = "https://api.github.com"
+  http_url         = "https://github.com"
+  oauth_token      = var.github_oauth_token
+  service_provider = "github"
+}
+
+locals {
+  workspaces = {
+    terraform-cloud = {
+      description = "Terraform Cloud organization and workspaces"
+      vcs_repo = {
+        identifier = "readycloudconsulting/terraform-cloud"
+        branch     = "main"
+      }
+    }
+  }
+}
+
+resource "tfe_workspace" "this" {
+  for_each = local.workspaces
+
+  name                  = each.key
+  description           = try(each.key["description"], null)
+  organization          = tfe_organization.this.name
+  file_triggers_enabled = false
+
+  dynamic "vcs_repo" {
+    for_each = [each.value.vcs_repo]
+
+    content {
+      identifier     = vcs_repo.value.identifier
+      branch         = vcs_repo.value.branch
+      oauth_token_id = tfe_oauth_client.this.id
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "github_oauth_token" {
+  description = "The token string you were given by GitHub."
+  type        = string
+}
